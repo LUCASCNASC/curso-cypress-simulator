24cc9ff551420963091692b5e45eb7893f792cf4
Lucas Camargo <nascimentocamargolucas@gmail.com>
2025-11-30 13:10:47 -0300

    Criando cenários de teste.

--- cypress/e2e/cypress-simulator.cy.js
+++ cypress/e2e/cypress-simulator.cy.js
@@ -1,5 +1,81 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+describe('Cypress Simulator', () => {
+
+  beforeEach(() => {
+
+    cy.visit('./src/index.html')
+
   })
+
+  it('sucess', () => {
+    
+  })
+
+  it('error: invalid cypress command', () => {
+    
+  })
+
+  it('warning', () => {
+    
+  })
+
+  it('error: valid command without parentheses', () => {
+    
+  })
+
+  it('help', () => {
+    
+  })
+
+  it('maximaze/minimize', () => {
+    
+  })
+
+  it('logout', () => {
+    
+  })
+
+  it('show and hide logout button', () => {
+    
+  })
+
+  it('runnig ... state', () => {
+    
+  })
+
+  it('accept cookies', () => {
+    
+  })
+
+  it('decline cookies', () => {
+    
+  })
+
+  it('captche button states', () => {
+    
+  })
+
+  it('captche error', () => {
+    
+  })
+
+  it('run button - enabled/disabled states', () => {
+    
+  })
+
+  it('reset textarea on logout and login', () => {
+    
+  })
+
+  it('disabled run button on logout and login', () => {
+    
+  })
+
+  it('reset output on logout and login', () => {
+    
+  })
+
+  it('no cookies banner in the login page', () => {
+    
+  })
+
 })
